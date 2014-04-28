bd8ce9f99c62ce02e69e83d35064219bec69d42c
Tarek Ziade <tarek@ziade.org>
2014-04-28 11:37:34 +0200

    raise version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = ["Programming Language :: Python", mplv2,
 
 
 setup(name='konfig',
-      version='0.8',
+      version='0.9',
       url='https://github.com/mozilla-services/konfig',
       packages=find_packages(),
       long_description=README,
